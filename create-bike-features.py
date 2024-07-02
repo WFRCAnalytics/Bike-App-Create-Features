235ed170b6988f0a33d7d2c94b316f3d853ff5f3
Josh Reynolds <jdreynolds33@gmail.com>
2024-07-02 17:34:41 -0600

    initial commit, line directionality, feature priority

---
+++ create-bike-features.py
@@ -0,0 +1,11 @@
+# load roads features
+
+# subset roads where (BikeFeatureLeft != Null or '') or (BikeFeatureRight != Null or '')
+
+# subset roads where 'BIKE_PLN_L IS NOT NULL Or BIKE_PLN_R IS NOT NULL'
+
+
+
+# load trails features
+
+# 'DesignatedUses NOT IN ('Pedestrian') And Status NOT IN ('Future', 'Proposed', 'PROPOSED') And CartoCode NOT IN ('1 - Hiking Only', '7 - Steps')'
